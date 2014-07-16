ab193e7192c8d5a03a6aecb8f23d02e12996b6f0
Angie31 <angiekarina31@gmail.com>
2014-07-16 07:07:59 -0600

    Class

---
+++ Class/Class17_Lista/src/class17_lista/Class17_Lista.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package class17_lista;
+
+/**
+ *
+ * @author Angie
+ */
+public class Class17_Lista {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Lista oLista=new Lista();
+        oLista.Insertar("Efren","200111-4");
+        oLista.Insertar("Joss","366111-5");
+        oLista.Insertar("Navas","774455-8");
+        oLista.Impirmir();
+    }
+    
+}
---
+++ Class/Class17_Lista/src/class17_lista/NodoDeLista.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package class17_lista;
+
+/**
+ *
+ * @author Angie
+ */
+public class NodoDeLista {
+    
+    private String dato;
+    private NodoDeLista siguiente;
+    private String cuentaBancaria;
+    
+    public NodoDeLista()
+    {
+        dato="";
+        siguiente=null;
+    
+    }
+
+    /**
+     * @return the dato
+     */
+    public String getDato() {
+        return dato;
+    }
+
+    /**
+     * @param dato the dato to set
+     */
+    public void setDato(String dato) {
+        this.dato = dato;
+    }
+
+    /**
+     * @return the siguiente
+     */
+    public NodoDeLista getSiguiente() {
+        return siguiente;
+    }
+
+    /**
+     * @param siguiente the siguiente to set
+     */
+    public void setSiguiente(NodoDeLista siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    /**
+     * @return the cuentaBancaria
+     */
+    public String getCuentaBancaria() {
+        return cuentaBancaria;
+    }
+
+    /**
+     * @param cuentaBancaria the cuentaBancaria to set
+     */
+    public void setCuentaBancaria(String cuentaBancaria) {
+        this.cuentaBancaria = cuentaBancaria;
+    }
+    
+    
+}
